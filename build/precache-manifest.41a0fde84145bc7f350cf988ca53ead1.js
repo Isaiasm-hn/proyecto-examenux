8273bc1fa0ccea4b53114d2321c485965614cf3c
Isaiasm-hn <isaiasm_hn@unitec.edu>
2019-08-19 08:01:18 -0600

    Terminado

---
+++ build/precache-manifest.41a0fde84145bc7f350cf988ca53ead1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ac00ae96a51aee747b1a5e21d16df9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e3644f08e061b5fd68f",
+    "url": "/static/js/2.62c22e4b.chunk.js"
+  },
+  {
+    "revision": "44e1610b80996a5bc464",
+    "url": "/static/js/main.3773814d.chunk.js"
+  },
+  {
+    "revision": "21e52bf9fc237c798191",
+    "url": "/static/js/runtime~main.2d884441.js"
+  }
+]);
