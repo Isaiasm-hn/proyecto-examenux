1581f9735acd50ba4aa463c239eda7c3c6d6ef04
Isaiasm-hn <isaiasm_hn@unitec.edu>
2019-08-19 01:21:47 -0600

    Avanzando 1.9

--- src/App.js
+++ src/App.js
@@ -34,18 +34,25 @@ function App() {
       </div>
       <div class="container">
         <div class="row">
-          
+        <div class="col-sm-4">
+            <h3>Purina Pro Plan</h3>
+            <img src=" https://www.purina.com/sites/g/files/auxxlc196/files/SPORT_ALS_PERF3020_ChknRc_F_18.png" class="rounded-circle" alt="Cinque Terre" width="209" height="229"></img>
+            <p>Chicken, corn gluten meal, brewers rice, animal fat preserved with mixed-tocopherols, poultry by-product meal (source of glucosamine), whole grain corn, corn germ meal, fish meal (source of glucosamine), animal digest, fish oil, dried egg product, salt, calcium carbonate, potassium chloride, calcium phosphate, Vitamin E supplement, choline chloride, L-Lysine monohydrochloride, L-ascorbyl-2-polyphosphate (source of Vitamin C), zinc sulfate, ferrous sulfate, manganese sulfate, niacin, Vitamin A supplement, calcium pantothenate, thiamine mononitrate, copper sulfate, riboflavin supplement, Vitamin B-12 supplement, pyridoxine hydrochloride, garlic oil, folic acid, Vitamin D-3 supplement, calcium iodate, biotin, menadione sodium bisulfite complex (Vitamin K), sodium selenite.   X-4461</p>
+          </div>
+ 
 
           <div class="col-sm-4">
             <h3>Pro Pac</h3>
             <img src="https://www.propacultimates.com/wp-content/uploads/2014/06/packaging-web4.png" class="rounded-circle" alt="Cinque Terre"width="209" height="229"></img>
             <p>PRO PAC® Ultimates™ Large Breed Adult Chicken Meal & Brown Rice Formula is scientifically formulated to meet the special needs of dogs that weigh over 50 lbs. A precise balance of protein, fat and carbohydrates promotes the special diet large and giant breed dogs need for overall good health. Fortified with glucosamine and chondroitin sulfate, PRO PAC® Ultimates™ Large Breed Adult Chicken Meal & Brown Rice Formula helps maintain joint health. Made without corn, wheat or soy, PRO PAC® Ultimates™ Large Breed Adult provides the ultimate in canine nutrition. This antioxidant-rich formula is fortified with Vitamin A and Vitamin C along with nutrient-rich vegetables and fruits from peas, apples, blueberries, carrots, spinach and cranberries. Fibers from beets, blueberries and cranberries help overall digestion. PRO PAC® Ultimates™ </p>
           </div>
+
           <div class="col-sm-4">
             <h3>NutriSource</h3>
             <img src="https://cdn.nutrisourcepetfoods.com/home/1.png" class="rounded-circle" alt="Cinque Terre" width="209" height="229"></img>
             <p>NutriSource® Grain Free High Plains Select formula is ideal for all breeds and all life stages. NutriSource® Grain Free High Plains Select formula delivers super premium nutrition in a holistically formulated, easy-to-digest, low glycemic food. We combine fresh USA Beef, USA Trout, low-ash Turkey meal and North American farmed garbanzo beans with carefully selected holistic ingredients to promote balanced growth and unparalleled health for your best friend. Health of your beloved pet is our #1 priority and our unmatched use of Alltech Good 4 Life supplements guarantees you won't have any gas or runny stool issues and a healthier immune system, we guarantee it. Bio-availability is important to your pets long-term health so we created a highly digestible, nutrient dense morsel with +85% digestibility rating. With NutriSource® you can feed less and get more nutrition - Health begins here!</p>
           </div>
+          
           <div class="col-sm-4">
             <h3>NutriSource</h3>
             <img src="https://www.royalcanin.com/us/-/media/royal-canin/united-states/images/products/dogs/sporting-life-agility-4100-dry-dog-food/ysdqgziudrtl6qifjyur.jpg?mw=250&hash=D788D6C125AC6233D0E2DDF5DA46EBD5342C4F9D" class="rounded-circle" alt="Cinque Terre" width="209" height="229"></img>
